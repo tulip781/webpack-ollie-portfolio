c287f33d21515950478d8cb01fde8d0c4cecfd06
Oliver Inglis <ollie@theinglistwins.com>
2019-12-13 23:47:19 +0000

    current state'

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,3 +1,6 @@
 setTimeout(fade = () => {
   document.querySelector('.load').classList.add('hidden')
+  setTimeout(show = () => {
+    document.querySelector('.land').classList.add('reveal')
+  }, 1000)
 }, 2000)
--- scripts/random_color.js
+++ scripts/random_color.js
@@ -1,7 +1,10 @@
 getNewRandomColor = () => {
-  const myArray = ['#ebe2da', '#d8def3', '#dddddd', '#dbc8bc', '#958e7d', '#96af76', '#cf9c82', '##D3D3D3'];
+  const myArray = ['#ebe2da', '#d8def3', '#dddddd', '#dbc8bc', '#958e7d', '#96af76', '#cf9c82', '#D3D3D3'];
   let rand = myArray[Math.floor(Math.random() * myArray.length)];
+  console.log(rand)
   document.getElementById("body").style.backgroundColor = rand;
+  document.getElementById("wrapper").style.backgroundColor = rand;
+  document.querySelector('.navbar').style.backgroundColor = rand;
 }
 
 getNewRandomColor();
