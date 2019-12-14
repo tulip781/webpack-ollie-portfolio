9a1d512255fe2b4ec571419c37c8302de9a0c3c3
Oliver Inglis <ollie@theinglistwins.com>
2019-12-14 11:32:31 +0000

    picture change

--- scripts/random_color.js
+++ scripts/random_color.js
@@ -1,5 +1,5 @@
 getNewRandomColor = () => {
-  const myArray = ['#ebe2da', '#d8def3', '#dddddd', '#dbc8bc', '#958e7d', '#96af76', '#cf9c82', '#D3D3D3'];
+  const myArray = ['#ebe2da', '#dddddd', '#dbc8bc', '#958e7d', '#96af76', '#cf9c82', '#D3D3D3'];
   let rand = myArray[Math.floor(Math.random() * myArray.length)];
   console.log(rand)
   document.getElementById("body").style.backgroundColor = rand;
