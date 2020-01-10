1cca237feb9ca4179bf37fbfdff977c4cbacfa1c
Oliver Inglis <ollie@theinglistwins.com>
2020-01-10 14:55:05 +0000

    fixes

--- scripts/code_page_color.js
+++ scripts/code_page_color.js
@@ -3,8 +3,8 @@ bgColour = () => {
  // if (sessionStorage.isVisited) document.querySelector('.navbar').style.opacity = '-webkit-opacity: 1';
  if (document.getElementById("about-body")) document.getElementById('about-body').style.backgroundColor = localStorage.getItem("usercolour");
  if (document.getElementById("big-html")) document.getElementById('big-html').style.backgroundColor = localStorage.getItem("usercolour");
+ document.querySelector('.navbar').style.backgroundColor = localStorage.getItem("usercolour");
  console.log('checking if i can find')
- if (sessionStorage.isVisited) document.querySelector('.navbar').style.backgroundColor = localStorage.getItem("usercolour");
 }
 
 bgColour();
