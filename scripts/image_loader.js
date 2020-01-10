b432a76014739679a411482110012041fc0fcd0f
Oliver Inglis <ollie@theinglistwins.com>
2020-01-10 14:42:50 +0000

    css removed

--- scripts/image_loader.js
+++ scripts/image_loader.js
@@ -1,3 +1,3 @@
 const imageToLoad = document.querySelector(".show-image");
-imageToLoad.addEventListener("load", imageToLoad.classList.add('loader');
+imageToLoad.addEventListener("load", imageToLoad.classList.add('loader'));
 console.log('this is working');
