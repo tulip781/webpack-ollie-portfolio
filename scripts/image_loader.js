f7a1c4bff2dff54eff150b404089896777a3cef9
Oliver Inglis <ollie@theinglistwins.com>
2020-01-10 17:53:33 +0000

    button changes

--- scripts/image_loader.js
+++ scripts/image_loader.js
@@ -1,10 +1,8 @@
 const action = () => {
   imageToLoad.classList.add('loader');
-  textBeneath.classList.add('loader');
 }
 
 const imageToLoad = document.querySelector(".show-image");
-const textBeneath = document.querySelector(".project-image-description");
 imageToLoad.addEventListener("load", action());
 console.log('the new funciton is working');
 
