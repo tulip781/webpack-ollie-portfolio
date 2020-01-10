14b7f64e1507bbb4c475997a65a5deeaf5d17d84
Oliver Inglis <ollie@theinglistwins.com>
2020-01-10 14:22:36 +0000

    load image error

--- scripts/image_loader.js
+++ scripts/image_loader.js
@@ -1,2 +1,3 @@
-document.querySelector(".show-image").classList.add('loader');
-
+const imageToLoad = document.querySelector(".show-image");
+imageToLoad.addEventListener("load", imageToLoad.classList.add('loader');
+console.log('this is working');
