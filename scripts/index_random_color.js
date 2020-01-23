bfab8ee0f397997e2112ea299400d5690d4a99c2
Oliver Inglis <ollie@theinglistwins.com>
2020-01-23 10:51:40 +0000

    javascript fixes

--- scripts/index_random_color.js
+++ scripts/index_random_color.js
@@ -1,22 +1,22 @@
-getNewRandomColor = () => {
-  const myArray = ['#ebe2da', '#dddddd', '#dbc8bc', '#958e7d', '#D3D3D3', "#ECE5BD"];
-  let rand = myArray[Math.floor(Math.random() * myArray.length)];
-  document.getElementById("body").style.backgroundColor = rand;
-  document.querySelector('.navbar').style.backgroundColor = rand;
-  let colour = document.getElementById("body").style.backgroundColor;
-  localStorage.setItem("usercolour",colour);
-}
+// getNewRandomColor = () => {
+//   const myArray = ['#ebe2da', '#dddddd', '#dbc8bc', '#958e7d', '#D3D3D3', "#ECE5BD"];
+//   let rand = myArray[Math.floor(Math.random() * myArray.length)];
+//   document.getElementById("body").style.backgroundColor = rand;
+//   document.querySelector('.navbar').style.backgroundColor = rand;
+//   let colour = document.getElementById("body").style.backgroundColor;
+//   localStorage.setItem("usercolour",colour);
+// }
 
-if (!sessionStorage.isVisited) {
-  getNewRandomColor();
-} else {
-    if (document.getElementById("about-body")) document.getElementById("about-body").style.backgroundColor = localStorage.getItem("usercolour");
-    document.getElementById("body").style.backgroundColor = localStorage.getItem("usercolour");
-    document.querySelector('.navbar').style.backgroundColor = localStorage.getItem("usercolour");
-}
+// if (!sessionStorage.isVisited) {
+//   getNewRandomColor();
+// } else {
+//     if (document.getElementById("about-body")) document.getElementById("about-body").style.backgroundColor = localStorage.getItem("usercolour");
+//     document.getElementById("body").style.backgroundColor = localStorage.getItem("usercolour");
+//     document.querySelector('.navbar').style.backgroundColor = localStorage.getItem("usercolour");
+// }
 
-const shuffle = document.querySelector('.shuffle');
+// const shuffle = document.querySelector('.shuffle');
 
-shuffle.addEventListener('click', (event) => {
-  getNewRandomColor()
-});
+// shuffle.addEventListener('click', (event) => {
+//   getNewRandomColor()
+// });
--- scripts/load_screen.js
+++ scripts/load_screen.js
@@ -1,7 +1,16 @@
+getNewRandomColor = () => {
+  const myArray = ['#ebe2da', '#dddddd', '#dbc8bc', '#958e7d', '#D3D3D3', "#ECE5BD"];
+  let rand = myArray[Math.floor(Math.random() * myArray.length)];
+  document.getElementById("body").style.backgroundColor = rand;
+  document.querySelector('.navbar').style.backgroundColor = rand;
+  let colour = document.getElementById("body").style.backgroundColor;
+  localStorage.setItem("usercolour",colour);
+}
 if (!sessionStorage.isVisited) {
+  getNewRandomColor();
   document.querySelector('.navbar').classList.add('hidden');
   document.getElementById('myNavbar').style.opacity = '0%';
-    document.getElementById('myNavbar').style.opacity = '-webkit-opacity: 0';
+  document.getElementById('myNavbar').style.opacity = '-webkit-opacity: 0';
   document.querySelector('.navbar').style.opacity = '0%';
   document.querySelector('.navbar').style.opacity = '-webkit-opacity: 0';
   document.querySelector('.load').style.opacity = '100%';
@@ -31,6 +40,15 @@ if (!sessionStorage.isVisited) {
     document.querySelector('.navbar').style.opacity = '100%';
     document.querySelector('.navbar').style.opacity = '-webkit-opacity: 1';
     document.querySelector('.navbar').classList.remove('hidden');
+    if (document.getElementById("about-body")) document.getElementById("about-body").style.backgroundColor = localStorage.getItem("usercolour");
+    document.getElementById("body").style.backgroundColor = localStorage.getItem("usercolour");
+    document.querySelector('.navbar').style.backgroundColor = localStorage.getItem("usercolour");
   }
 }
 
+
+const shuffle = document.querySelector('.shuffle');
+
+shuffle.addEventListener('click', (event) => {
+  getNewRandomColor()
+});
